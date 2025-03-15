dd24da231789b20141d3c4fe64053f8e4be8a474
Soobin Yang <soobin.yang@osc.ai>
2025-03-15 15:30:27 -0400

    set model name as constant

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from './generative-ai.js';
+import { MODEL_NAMES } from './constants.js';
 
 const safetySetting = [
   {
@@ -24,8 +25,8 @@ let genAI, modelFlash, modelPro;
 chrome.storage.local.get("apiKey", (result) => {
   if (result.apiKey) {
     let genAI = new GoogleGenerativeAI(result.apiKey);
-    modelFlash = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySetting });
-    modelPro = genAI.getGenerativeModel({ model: "gemini-1.5-pro", safetySetting });
+    modelFlash = genAI.getGenerativeModel({ model: MODEL_NAMES.FLASH, safetySetting });
+    modelPro = genAI.getGenerativeModel({ model: MODEL_NAMES.PRO, safetySetting });
   }
 });
 
@@ -34,8 +35,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (modelFlash === undefined || modelPro === undefined) {
       chrome.storage.local.get(["apiKey"]).then((result) => {
         genAI = new GoogleGenerativeAI(result.apiKey);
-        modelFlash = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySetting });
-        modelPro = genAI.getGenerativeModel({ model: "gemini-1.5-pro", safetySetting });
+        modelFlash = genAI.getGenerativeModel({ model: MODEL_NAMES.FLASH, safetySetting });
+        modelPro = genAI.getGenerativeModel({ model: MODEL_NAMES.PRO, safetySetting });
       });
     }
     const model = request.model === "flash" ? modelFlash : modelPro;
@@ -49,8 +50,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const API_KEY = request.apiKey;
     chrome.storage.local.set({ "apiKey": API_KEY }).then(() => {
       genAI = new GoogleGenerativeAI(API_KEY);
-      modelFlash = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySetting });
-      modelPro = genAI.getGenerativeModel({ model: "gemini-1.5-pro", safetySetting });
+      modelFlash = genAI.getGenerativeModel({ model: MODEL_NAMES.FLASH, safetySetting });
+      modelPro = genAI.getGenerativeModel({ model: MODEL_NAMES.PRO, safetySetting });
       sendResponse({ success: true });
     });
   }
